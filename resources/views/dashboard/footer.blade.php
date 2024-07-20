b6de11054a73d6114b2c125f68e90ff1e5c93cce
hanthinzaraung <hanthinzar1@gmail.com>
2024-07-20 14:02:38 +0630

    Demo.js Updated

--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -57,7 +57,7 @@
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('dist/js/demo.js ')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<!-- <script src="{{ asset('dist/js/pages/dashboard.js ')}}"></script> -->
+<script src="{{ asset('dist/js/pages/dashboard.js ')}}"></script>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
